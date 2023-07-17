551ba4fd5d87a723d35bfef3dbf73e74e32f8c19
Rune Vium Søndergaard <rvso@netcompany.com>
2023-07-17 22:53:23 +0200

    Working very nicely with bootstrap file

--- Program.cs
+++ Program.cs
@@ -1,68 +1,119 @@
-﻿using Sharprompt;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Sharprompt;
 using ShellProgressBar;
 
 internal class Program
 {
-    /// <summary>
-    ///     Entry point of the application.
-    /// </summary>
     private static async Task Main()
     {
         try
         {
-            var txtFiles = GetTextFiles();
+            while (true)
+            {
+                var bootstrapFilePath = FindBootstrapFile();
+                var options = ExtractOptionsFromBootstrapFile(bootstrapFilePath);
 
-            var pageSize = GetOptimalPageSize();
-            var selectedFiles = SelectFiles(txtFiles, pageSize);
+                var selectedFile = SelectOption(options);
 
-            using var mainProgressBar = CreateMainProgressBar(selectedFiles.Length);
-            await DownloadFiles(selectedFiles, mainProgressBar);
+                var selectedFiles = PromptForFiles(selectedFile);
 
-            Console.WriteLine("Download af alle episoder er fuldført :D (tryk på enter for at lukke)");
+                using var mainProgressBar = CreateMainProgressBar(selectedFiles.Count);
+                await DownloadFiles(selectedFiles, mainProgressBar);
+
+                Console.WriteLine("Download of all episodes completed :D");
+                Console.WriteLine();
+
+                var continueChoice = Prompt.Confirm("Do you want to continue downloading files from other lists?");
+                if (!continueChoice)
+                    break;
+            }
+
+            Console.WriteLine("All downloads completed. Press enter to exit.");
             Console.ReadLine();
         }
         catch (Exception e)
         {
-            Console.WriteLine("Noget gik galt! Se fejlen nedenfor");
-            Console.WriteLine("Genereret fejlbesked: " + e.Message);
+            Console.WriteLine("Something went wrong! See the error message below:");
+            Console.WriteLine("Error message: " + e.Message);
             Console.ReadLine();
         }
     }
 
-    /// <summary>
-    ///     Retrieves the list of text files in the current directory.
-    /// </summary>
-    /// <returns>The array of text file paths.</returns>
-    private static IEnumerable<string> GetTextFiles()
+    private static string FindBootstrapFile()
     {
-        var txtFiles = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.txt")
-            .Select(Path.GetFileName).ToArray();
+        while (true)
+        {
+            var bootstrapFile = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "__Bootstrap.txt")
+                .FirstOrDefault();
 
-        if (txtFiles.Length == 0)
-            throw new Exception("Placer venligst minimum 1 dataliste (txt-fil) ved siden af exe-filen");
+            if (bootstrapFile != null)
+                return bootstrapFile;
 
-        return txtFiles!;
+            Console.WriteLine("The __Bootstrap.txt file was not found. Please make sure it exists.");
+            Console.WriteLine("Press enter to retry...");
+            Console.ReadLine();
+        }
     }
 
-    /// <summary>
-    ///     Prompts the user to select files from the provided list.
-    /// </summary>
-    /// <param name="txtFiles">The array of text file paths.</param>
-    /// <param name="pageSize">The page size for displaying prompts.</param>
-    /// <returns>The array of selected file paths.</returns>
-    private static string[] SelectFiles(IEnumerable<string> txtFiles, int pageSize)
+    private static List<FileOption> ExtractOptionsFromBootstrapFile(string filePath)
     {
-        return Prompt.MultiSelect(
-            "Brug venstre/højre pil for at navigere sider. Marker med mellemrum og tryk enter for at bekræfte",
-            txtFiles,
-            pageSize).ToArray();
+        var options = new List<FileOption>();
+
+        var lines = File.ReadAllLines(filePath);
+        foreach (var line in lines)
+        {
+            var content = line.Split('\t');
+            if (content.Length == 2)
+            {
+                var url = content[0];
+                var path = content[1];
+                options.Add(new FileOption { Url = url, Path = path });
+            }
+        }
+
+        if (options.Count == 0)
+            throw new Exception("No valid options were found in the file: " + filePath);
+
+        return options;
     }
 
-    /// <summary>
-    ///     Creates the main progress bar for the file download process.
-    /// </summary>
-    /// <param name="totalFiles">The total number of files to download.</param>
-    /// <returns>The created main progress bar.</returns>
+    private static FileOption SelectOption(List<FileOption> options)
+    {
+        var optionTexts = options.Select(option => Path.GetFileName(option.Path)).ToArray();
+        var selectedIndex = Prompt.Select("Choose a file to download:", optionTexts);
+        var selectedOption = options[Array.IndexOf(optionTexts, selectedIndex)];
+
+        if (!File.Exists(selectedOption.Path))
+        {
+            using var client = new HttpClient();
+            var response = client.GetAsync(selectedOption.Url).Result;
+            response.EnsureSuccessStatusCode();
+            var content = response.Content.ReadAsByteArrayAsync().Result;
+            File.WriteAllBytes(selectedOption.Path, content);
+        }
+
+        return selectedOption;
+    }
+
+    private static List<FileOption> PromptForFiles(FileOption selectedFile)
+    {
+        var selectedFileOptions = ExtractOptionsFromBootstrapFile(selectedFile.Path);
+        var filesToDownload = selectedFileOptions.Except(new[] { selectedFile });
+
+        var optionTexts = filesToDownload.Select(option => Path.GetFileName(option.Path)).ToArray();
+        var selectedOptionTexts = Prompt.MultiSelect("Choose files to download:", optionTexts);
+
+        var selectedFiles = filesToDownload.Where(option => selectedOptionTexts.Contains(Path.GetFileName(option.Path))).ToList();
+
+        return selectedFiles;
+    }
+
+
     private static ProgressBar CreateMainProgressBar(int totalFiles)
     {
         var mainProgressBarOptions = new ProgressBarOptions
@@ -75,27 +126,15 @@ internal class Program
         return new ProgressBar(totalFiles, "Downloading Files", mainProgressBarOptions);
     }
 
-    /// <summary>
-    ///     Downloads the selected files and updates the main progress bar.
-    /// </summary>
-    /// <param name="selectedFiles">The array of selected file paths.</param>
-    /// <param name="mainProgressBar">The main progress bar.</param>
-    /// <returns>The task representing the download process.</returns>
-    private static async Task DownloadFiles(IReadOnlyCollection<string> selectedFiles, ProgressBarBase mainProgressBar)
+    private static async Task DownloadFiles(List<FileOption> options, ProgressBarBase mainProgressBar)
     {
-        var tasks = selectedFiles.Select(file => ProcessFileAsync(file, mainProgressBar)).ToArray();
+        var tasks = options.Select(option => ProcessFileAsync(option, mainProgressBar)).ToArray();
         await Task.WhenAll(tasks);
-        mainProgressBar.Tick(selectedFiles.Count); // Update the main progress bar to 100%
+        mainProgressBar.Tick(options.Count); // Update the main progress bar to 100%
     }
 
-    /// <summary>
-    ///     Processes the specified file asynchronously and updates the child progress bar.
-    /// </summary>
-    /// <param name="file">The file to process.</param>
-    /// <param name="mainProgressBar">The main progress bar.</param>
-    private static async Task ProcessFileAsync(string? file, ProgressBarBase mainProgressBar)
+    private static async Task ProcessFileAsync(FileOption option, ProgressBarBase mainProgressBar)
     {
-        var lines = await File.ReadAllLinesAsync(file);
         var childProgressBarOptions = new ProgressBarOptions
         {
             ForegroundColor = ConsoleColor.Yellow,
@@ -103,8 +142,23 @@ internal class Program
             ProgressCharacter = '─'
         };
 
-        using var childProgressBar = mainProgressBar.Spawn(lines.Length, "Downloading: " + Path.GetFileName(file),
+        var tempFilePath = Path.GetTempFileName();
+
+        if (!File.Exists(option.Path))
+        {
+            using var client = new HttpClient();
+            using var response = await client.GetAsync(option.Url);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsByteArrayAsync();
+            File.WriteAllBytes(tempFilePath, content);
+        }
+
+        var lines = await File.ReadAllLinesAsync(tempFilePath);
+        File.Delete(tempFilePath);
+
+        using var childProgressBar = mainProgressBar.Spawn(lines.Length, "Downloading: " + Path.GetFileName(option.Path),
             childProgressBarOptions);
+
         foreach (var line in lines)
         {
             var content = line.Split('\t');
@@ -121,16 +175,26 @@ internal class Program
 
             if (directoryName != null)
             {
-                using var client = new HttpClient();
-                await using var s = await client.GetStreamAsync(url);
-                var tempfile = Path.GetTempFileName();
-                await using (var fs = new FileStream(tempfile, FileMode.OpenOrCreate))
+                try
                 {
-                    await s.CopyToAsync(fs);
-                }
+                    using var client2 = new HttpClient();
+                    using var response2 = await client2.GetAsync(url);
+                    response2.EnsureSuccessStatusCode();
+                    var contentStream2 = await response2.Content.ReadAsStreamAsync();
 
-                Directory.CreateDirectory(directoryName);
-                File.Move(tempfile, path);
+                    var tempfile2 = Path.GetTempFileName();
+                    await using (var fs2 = new FileStream(tempfile2, FileMode.OpenOrCreate))
+                    {
+                        await contentStream2.CopyToAsync(fs2);
+                    }
+
+                    Directory.CreateDirectory(directoryName);
+                    File.Move(tempfile2, path);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"WARNING: Unable to download file from {url}. Error: {ex.Message}. Skipping download.");
+                }
             }
 
             childProgressBar.Tick();
@@ -138,16 +202,10 @@ internal class Program
 
         mainProgressBar.Tick(); // Update the main progress bar after all child progress is complete
     }
+}
 
-    /// <summary>
-    ///     Calculates the optimal page size for displaying prompts.
-    /// </summary>
-    /// <returns>The optimal page size.</returns>
-    private static int GetOptimalPageSize()
-    {
-        var windowHeight = Console.WindowHeight;
-        var pageSize = windowHeight - 5;
-
-        return pageSize > 0 ? pageSize : 1;
-    }
+internal class FileOption
+{
+    public string Url { get; set; }
+    public string Path { get; set; }
 }
