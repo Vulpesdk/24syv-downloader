dc3bb5c7b5c6c42204b83940e6ee4c486962d2ce
Rune Vium Søndergaard <rvso@netcompany.com>
2023-07-20 12:10:25 +0200

    Logic now less flawed and simpler to understand

--- Program.cs
+++ Program.cs
@@ -1,22 +1,51 @@
 ﻿using Sharprompt;
 using ShellProgressBar;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
+/// <summary>
+/// Represents a file option with URL and path information.
+/// </summary>
+internal class FileOption
+{
+    /// <summary>
+    /// Gets or sets the URL of the file to be downloaded.
+    /// </summary>
+    public string? Url { get; set; }
+
+    /// <summary>
+    /// Gets or sets the local path where the file will be saved.
+    /// </summary>
+    public string? Path { get; set; }
+}
+
+/// <summary>
+/// Represents the main program class.
+/// </summary>
 internal class Program
 {
     private static readonly string BootstrapUrl = "https://denkortewiki.dk/lister/__Bootstrap.txt";
 
+    /// <summary>
+    /// The main entry point of the program.
+    /// </summary>
     private static async Task Main()
     {
         try
         {
             while (true)
             {
-                var options = await ExtractOptionsFromBootstrapFile();
+                var bootstrapOptions = await FetchBootstrapOptions();
+                var selectedOption = SelectBootstrapOption(bootstrapOptions);
 
-                var selectedFile = SelectOption(options);
+                var availableFiles = ProcessBootstrapContent(selectedOption);
 
                 var pageSize = GetOptimalPageSize();
-                var selectedFiles = PromptForFiles(selectedFile, pageSize);
+                var selectedFiles = PromptForFiles(availableFiles, pageSize);
 
                 using var mainProgressBar = CreateMainProgressBar(selectedFiles.Count);
                 await DownloadFiles(selectedFiles, mainProgressBar);
@@ -41,11 +70,10 @@ internal class Program
     }
 
     /// <summary>
-    ///     Extracts FileOption list from the given file.
+    /// Fetches the bootstrap options from the remote server.
     /// </summary>
-    /// <param name="filePath">The path of the bootstrap file.</param>
-    /// <returns>List of FileOptions.</returns>
-    private static async Task<List<FileOption>> ExtractOptionsFromBootstrapFile()
+    /// <returns>A list of bootstrap file options.</returns>
+    private static async Task<List<FileOption>> FetchBootstrapOptions()
     {
         var options = new List<FileOption>();
 
@@ -73,69 +101,76 @@ internal class Program
     }
 
     /// <summary>
-    ///     Selects a FileOption from the provided list of options.
+    /// Selects a bootstrap option from the available options.
     /// </summary>
-    /// <param name="options">List of FileOptions to choose from.</param>
-    /// <returns>The selected FileOption.</returns>
-    private static FileOption SelectOption(List<FileOption> options)
+    /// <param name="bootstrapOptions">The list of available bootstrap options.</param>
+    /// <returns>The selected bootstrap file option.</returns>
+    private static FileOption SelectBootstrapOption(List<FileOption> bootstrapOptions)
     {
-        var optionTexts = options.Select(option => Path.GetFileName(option.Path)).ToArray();
+        var optionTexts = bootstrapOptions.Select(option => Path.GetFileName(option.Url)).ToArray();
         var selectedIndex = Prompt.Select("Choose a file to download", optionTexts);
-        var selectedOption = options[Array.IndexOf(optionTexts, selectedIndex)];
-
-        if (!File.Exists(selectedOption.Path))
-        {
-            using var client = new HttpClient();
-            var response = client.GetAsync(selectedOption.Url).Result;
-            response.EnsureSuccessStatusCode();
-            var content = response.Content.ReadAsByteArrayAsync().Result;
-            File.WriteAllBytes(selectedOption.Path, content);
-        }
+        var selectedOption = bootstrapOptions[Array.IndexOf(optionTexts, selectedIndex)];
 
         return selectedOption;
     }
 
     /// <summary>
-    ///     Prompts the user to select files for downloading from the provided FileOption list.
+    /// Processes the content of the selected bootstrap option and returns a list of file options.
     /// </summary>
-    /// <param name="selectedFile">The initially selected FileOption.</param>
-    /// <param name="pageSize">The optimal page size for displaying prompts.</param>
-    /// <returns>List of selected FileOptions.</returns>
-    private static List<FileOption> PromptForFiles(FileOption selectedFile, int pageSize)
+    /// <param name="selectedBootstrapOption">The selected bootstrap option.</param>
+    /// <returns>A list of file options.</returns>
+    private static List<FileOption> ProcessBootstrapContent(FileOption selectedBootstrapOption)
     {
-        var lines = File.ReadAllLines(selectedFile.Path);
+        if (selectedBootstrapOption.Url == null)
+            return new List<FileOption>();
 
-        var options = new List<FileOption>();
-        foreach (var line in lines)
+        using var client = new HttpClient();
+        var response = client.GetAsync(selectedBootstrapOption.Url).Result;
+        response.EnsureSuccessStatusCode();
+        var content = response.Content.ReadAsStringAsync().Result;
+
+        var lines = content.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+        return lines.Select(line =>
         {
-            var content = line.Split('\t');
-            if (content.Length == 2)
+            var parts = line.Split('\t');
+            if (parts.Length == 2)
             {
-                var url = content[0];
-                var path = content[1];
-                options.Add(new FileOption { Url = url, Path = path });
+                var url = parts[0];
+                var path = parts[1];
+                return new FileOption { Url = url, Path = path };
             }
-        }
+            return null;
+        })
+        .Where(option => option != null)
+        .ToList();
+    }
 
-        var filesToDownload = options.Except(new[] { selectedFile });
-
-        var optionTexts = filesToDownload.Select(option => Path.GetFileName(option.Path)).ToArray();
+    /// <summary>
+    /// Prompts the user to choose files to download from the available files.
+    /// </summary>
+    /// <param name="availableFiles">The list of available files.</param>
+    /// <param name="pageSize">The page size for pagination.</param>
+    /// <returns>The list of selected files.</returns>
+    private static List<FileOption> PromptForFiles(List<FileOption> availableFiles, int pageSize)
+    {
+        var optionTexts = availableFiles.Select(option => Path.GetFileName(option.Url)).ToArray();
         var selectedOptionTexts =
             Prompt.MultiSelect("Choose files to download, use left/right arrow keys for pagination", optionTexts,
                 pageSize);
 
-        var selectedFiles = filesToDownload
-            .Where(option => selectedOptionTexts.Contains(Path.GetFileName(option.Path)))
+        var selectedFiles = availableFiles
+            .Where(option => selectedOptionTexts.Contains(Path.GetFileName(option.Url)))
             .ToList();
 
         return selectedFiles;
     }
 
     /// <summary>
-    ///     Creates a progress bar for the main download process.
+    /// Creates the main progress bar.
     /// </summary>
-    /// <param name="totalFiles">The total number of files to download.</param>
-    /// <returns>A new ProgressBar instance.</returns>
+    /// <param name="totalFiles">The total number of files to be downloaded.</param>
+    /// <returns>The main progress bar.</returns>
     private static ProgressBar CreateMainProgressBar(int totalFiles)
     {
         var mainProgressBarOptions = new ProgressBarOptions
@@ -149,11 +184,11 @@ internal class Program
     }
 
     /// <summary>
-    ///     Downloads the files from the provided list of options.
+    /// Downloads the selected files and updates the progress bar.
     /// </summary>
-    /// <param name="options">List of FileOptions to download.</param>
+    /// <param name="options">The list of selected file options.</param>
     /// <param name="mainProgressBar">The main progress bar.</param>
-    /// <returns>Task representing the asynchronous operation.</returns>
+    /// <returns>The task representing the asynchronous operation.</returns>
     private static async Task DownloadFiles(List<FileOption> options, ProgressBarBase mainProgressBar)
     {
         var tasks = options.Select(option => ProcessFileAsync(option, mainProgressBar)).ToArray();
@@ -162,11 +197,11 @@ internal class Program
     }
 
     /// <summary>
-    ///     Processes a file asynchronously, downloading its content and updating the progress.
+    /// Processes a single file asynchronously and updates the progress bar.
     /// </summary>
-    /// <param name="option">The FileOption to process.</param>
+    /// <param name="option">The file option to be processed.</param>
     /// <param name="mainProgressBar">The main progress bar.</param>
-    /// <returns>Task representing the asynchronous operation.</returns>
+    /// <returns>The task representing the asynchronous operation.</returns>
     private static async Task ProcessFileAsync(FileOption option, ProgressBarBase mainProgressBar)
     {
         var childProgressBarOptions = new ProgressBarOptions
@@ -178,13 +213,14 @@ internal class Program
 
         var tempFilePath = Path.GetTempFileName();
 
-        if (!File.Exists(option.Path))
+        using var client = new HttpClient();
+        using var response = await client.GetAsync(option.Url);
+        response.EnsureSuccessStatusCode();
+        await using var contentStream = await response.Content.ReadAsStreamAsync();
+
+        await using (var fs = new FileStream(tempFilePath, FileMode.Create))
         {
-            using var client = new HttpClient();
-            using var response = await client.GetAsync(option.Url);
-            response.EnsureSuccessStatusCode();
-            var content = await response.Content.ReadAsByteArrayAsync();
-            File.WriteAllBytes(tempFilePath, content);
+            await contentStream.CopyToAsync(fs);
         }
 
         var lines = await File.ReadAllLinesAsync(tempFilePath);
@@ -211,15 +247,15 @@ internal class Program
             if (directoryName != null)
                 try
                 {
-                    using var client = new HttpClient();
-                    using var response = await client.GetAsync(url);
-                    response.EnsureSuccessStatusCode();
-                    var contentStream = await response.Content.ReadAsStreamAsync();
+                    using var client2 = new HttpClient();
+                    using var response2 = await client2.GetAsync(url);
+                    response2.EnsureSuccessStatusCode();
+                    var contentStream2 = await response2.Content.ReadAsStreamAsync();
 
                     var tempfile = Path.GetTempFileName();
-                    await using (var fs = new FileStream(tempfile, FileMode.OpenOrCreate))
+                    await using (var fs2 = new FileStream(tempfile, FileMode.Create))
                     {
-                        await contentStream.CopyToAsync(fs);
+                        await contentStream2.CopyToAsync(fs2);
                     }
 
                     Directory.CreateDirectory(directoryName);
@@ -238,7 +274,7 @@ internal class Program
     }
 
     /// <summary>
-    ///     Calculates the optimal page size for displaying prompts.
+    /// Gets the optimal page size for pagination.
     /// </summary>
     /// <returns>The optimal page size.</returns>
     private static int GetOptimalPageSize()
@@ -248,10 +284,4 @@ internal class Program
 
         return pageSize > 0 ? pageSize : 1;
     }
-}
-
-internal class FileOption
-{
-    public string Url { get; set; }
-    public string Path { get; set; }
 }
